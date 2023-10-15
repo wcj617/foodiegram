289fbe2b95d2ff716188af12d11b687e0d5bf0ac
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2023-10-14 22:07:50 -0700

    search & image feature added

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import image_upload_view
 
-from .views import HomePageView
+from .views import HomePageView, FoodAutocomplete
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path('autocomplete/', FoodAutocomplete.as_view(), name='food-autocomplete'),
+    path('upload/', image_upload_view)
 ]
