14303be0559dc1917fc6c94172c2dcee2fa90118
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2023-10-14 20:13:26 -0700

    search bar

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model): 
+    name = models.CharField(max_length=100) 
+  
+    def __str__(self): 
+        return f"{self.name}"
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images')
+    def __str__(self):
+        return self.title
