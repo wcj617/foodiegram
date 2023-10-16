83eddac54a06a973f569ed37dbf579df1f2eb47e
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2023-10-15 17:41:20 -0700

    upload page styliing

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import validate_image_file_extension
 
 # Create your models here.
 class Image(models.Model):
     title = models.CharField(max_length=200, primary_key=True)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images',validators=[validate_image_file_extension])
 
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
--- pages/views.py
+++ pages/views.py
@@ -19,15 +19,77 @@ class FoodAutocomplete(View):
 from django.shortcuts import render
 from .forms import ImageForm
 
+import clarifai
+
 def image_upload_view(request):
     """Process images uploaded by users"""
+    #print("HEY")
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             # Get the current instance object to display in the template
             img_obj = form.instance
+            #print("LOL")
+            predict(img_obj.image.url)
             return render(request, 'upload.html', {'form': form, 'img_obj': img_obj})
+        else:
+            form = ImageForm()
+            return render(request, 'upload.html', {'form': form})
     else:
         form = ImageForm()
         return render(request, 'upload.html', {'form': form})
+    
+def predict(url):
+    # Your PAT (Personal Access Token) can be found in the portal under Authentification
+    PAT = 'c65c19a094404c88915f3889c8139bf3'
+    # Specify the correct user_id/app_id pairings
+    # Since you're making inferences outside your app's scope
+    USER_ID = 'clarifai'
+    APP_ID = 'main'
+    # Change these to whatever model and image URL you want to use
+    MODEL_ID = 'food-item-recognition'
+    MODEL_VERSION_ID = '1d5fd481e0cf4826aa72ec3ff049e044'
+    IMAGE_URL = url
+
+    from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+    from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+    from clarifai_grpc.grpc.api.status import status_code_pb2
+
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
+
+    metadata = (('authorization', 'Key ' + PAT),)
+
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+
+    post_model_outputs_response = stub.PostModelOutputs(
+        service_pb2.PostModelOutputsRequest(
+            user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
+            model_id=MODEL_ID,
+            version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        image=resources_pb2.Image(
+                            url=IMAGE_URL
+                        )
+                    )
+                )
+            ]
+        ),
+        metadata=metadata
+    )
+    if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+        print(post_model_outputs_response.status)
+        raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+
+    # Since we have one input, one output will exist here
+    output = post_model_outputs_response.outputs[0]
+
+    print("Predicted concepts:")
+    for concept in output.data.concepts:
+        print("%s %.2f" % (concept.name, concept.value))
+
+    # Uncomment this line to print the full Response JSON
+    print(output)
